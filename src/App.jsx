1d85f42cd4572e88f18f0a2f3c18366c2579fe62
ghazal <ghazalfatal.02@gmail.com>
2024-03-06 14:18:37 +0200

    creating the game board

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import Player from "./components/Player.jsx"
-
+import GameBoard from "./components/GameBoard.jsx"
 function App() {
   
 
@@ -13,7 +13,7 @@ function App() {
     
         </ol>
 
-        Game Board 
+        <GameBoard/>
       </div>
       LOG
     </main>
